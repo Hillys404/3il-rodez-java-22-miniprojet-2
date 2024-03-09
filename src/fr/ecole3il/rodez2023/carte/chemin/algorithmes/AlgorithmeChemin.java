6d7eed29efb08d8b95480da2f489b04f24ff4164
Jade Causse <75785236+Hillys404@users.noreply.github.com>
2024-03-09 10:18:04 +0100

    Ajout de commentaires pour l'interface AlgorithmeChemin.java

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
@@ -7,5 +7,13 @@ import java.util.List;
 
 public interface AlgorithmeChemin<E> {
 
+    /**
+     * Trouve le chemin (le plus court) entre un noeud départ
+     * et un noeud arrivé dans le graphe donné
+     * @param graphe graphe avec lequel on travaille
+     * @param depart noeud de départ
+     * @param arrivee noeud d'arrivée
+     * @return liste de noeuds qui constitue le chemin entre les deux noeuds
+     */
     List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee);
 }
